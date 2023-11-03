93a03f25f2e735800fcf8d56e2c80a37e6571429
AndrelGanShiTV <andresfelipegiraldoandrade@gmail.com>
2023-11-03 18:51:29 -0500

    Navigator

--- views/Menu.js
+++ views/Menu.js
@@ -2,11 +2,15 @@
 import React from 'react';
 import { SafeAreaView, View, Pressable, Text } from 'react-native';
 
-function Menu() {
+function Menu({ navigation }) {
   return (
     <SafeAreaView>
       <View>
-        <Pressable style={{ backgroundColor: "blue" }} >
+        <Pressable 
+          style={{ backgroundColor: "blue" }} 
+          onPress={()=>{
+          navigation.navigate('Training')
+        }}>
           <Text style={{ color: 'white' }}>Ver Entrenamientos</Text>
         </Pressable>
         <Pressable>
