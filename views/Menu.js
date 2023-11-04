3ec6d333a79b532ed5c6aa251b9c968efa727989
AndrelGanShiTV <andresfelipegiraldoandrade@gmail.com>
2023-11-03 19:38:34 -0500

    Estilos

--- views/Menu.js
+++ views/Menu.js
@@ -1,24 +1,44 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { SafeAreaView, View, Pressable, Text } from 'react-native';
+import { SafeAreaView, View, Pressable, Text, StyleSheet } from 'react-native';
 
 function Menu({ navigation }) {
+
   return (
     <SafeAreaView>
       <View>
         <Pressable 
-          style={{ backgroundColor: "blue" }} 
+          style={style.btn} 
           onPress={()=>{
-          navigation.navigate('Training')
-        }}>
-          <Text style={{ color: 'white' }}>Ver Entrenamientos</Text>
+            navigation.navigate('Training');
+          }}>
+          <Text style={style.btnText}>Ver Entrenamientos</Text>
         </Pressable>
-        <Pressable>
-          <Text>Cerrar Sesion</Text>
+        <Pressable
+          style={style.btn}
+        >
+          <Text style={style.btnText}>Cerrar Sesion</Text>
         </Pressable>
       </View>
     </SafeAreaView>
-  )
+  );
 }
+
+const style = StyleSheet.create({
+  btn: {
+    backgroundColor: '#6D28D9',
+    marginHorizontal: 40,
+    marginVertical: 10,
+    padding: 10,
+    borderRadius: 5,
+  },
+  btnText: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '500',
+    textTransform: 'uppercase',
+  },
+});
 
 export default Menu;
--- views/Training.js
+++ views/Training.js
@@ -1,15 +1,26 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {SafeAreaView,View,Text} from 'react-native';
+import {SafeAreaView,View,Text,StyleSheet} from 'react-native';
 
 function Training({ navigation }) {
   return (
     <SafeAreaView>
         <View>
-            <Text>Entrenamientos Programados</Text>
+            <Text style={style.title}>Entrenamientos Programados</Text>
         </View>
     </SafeAreaView>
-  )
+  );
 }
+
+const style = StyleSheet.create({
+  title: {
+    marginHorizontal: 10,
+    marginVertical: 10,
+    fontFamily: 'caption',
+    fontSize: 25,
+    color: 'black',
+    textAlign: 'center',
+  },
+});
 
 export default Training;
